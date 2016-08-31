eb575b0afec4cce5c58aa37e85223c9bc036f1fb
Carson.Ho <carson.ho@foxmail.com>
2016-08-31 16:45:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/carson_ho/webview_demo/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.carson_ho.webview_demo;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    WebView mWebview;
+    WebSettings mWebSettings;
+    TextView beginLoading,endLoading,loading,mtitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mWebview = (WebView) findViewById(R.id.webView1);
+        beginLoading = (TextView) findViewById(R.id.text_beginLoading);
+        endLoading = (TextView) findViewById(R.id.text_endLoading);
+        loading = (TextView) findViewById(R.id.text_Loading);
+        mtitle = (TextView) findViewById(R.id.title);
+
+        mWebSettings = mWebview.getSettings();
+
+        mWebview.loadUrl("http://www.baidu.com/");
+
+
+        //设置不用系统浏览器打开,直接显示在当前Webview
+        mWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        //设置WebChromeClient类
+        mWebview.setWebChromeClient(new WebChromeClient() {
+
+
+            //获取网站标题
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                System.out.println("标题在这里");
+                mtitle.setText(title);
+            }
+
+
+            //获取加载进度
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    String progress = newProgress + "%";
+                    loading.setText(progress);
+                } else if (newProgress == 100) {
+                    String progress = newProgress + "%";
+                    loading.setText(progress);
+                }
+            }
+        });
+
+
+        //设置WebViewClient类
+        mWebview.setWebViewClient(new WebViewClient() {
+            //设置加载前的函数
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                System.out.println("开始加载了");
+                beginLoading.setText("开始加载了");
+
+            }
+
+            //设置结束加载函数
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                endLoading.setText("结束加载了");
+
+            }
+        });
+    }
+
+    //点击返回上一页面而不是退出浏览器
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebview.canGoBack()) {
+            mWebview.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    //销毁Webview
+    @Override
+    protected void onDestroy() {
+        if (mWebview != null) {
+            mWebview.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            mWebview.clearHistory();
+
+            ((ViewGroup) mWebview.getParent()).removeView(mWebview);
+            mWebview.destroy();
+            mWebview = null;
+        }
+        super.onDestroy();
+    }
+}
